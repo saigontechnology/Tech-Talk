0a4586cd0430e4888158c4479b78203dda8b4300
hao.nguyen <hao.nguyen@saigontechnology.com>
2024-06-07 14:04:19 +0700

    remove db config

--- React/state-management/hao.nguyen/react-query/be-react-query/config/database.config.js
+++ React/state-management/hao.nguyen/react-query/be-react-query/config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://u70lq2dxkoceg5obdxec:3sNe1chVJHOaJEXvX9Zi@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bxzv0koujo5xkkt?replicaSet=rs0'
+    url: ''
 }
