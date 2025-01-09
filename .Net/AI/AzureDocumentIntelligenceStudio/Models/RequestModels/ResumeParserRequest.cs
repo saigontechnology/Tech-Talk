c0eb7c47ee63accb4bcd33e936a3bfdc362d99a5
Tran Tien Phat <phat.tran@saigontechnology.com>
2025-01-09 15:13:13 +0700

    Implement Resume Parser

---
+++ .Net/AI/AzureDocumentIntelligenceStudio/Models/RequestModels/ResumeParserRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureDocumentIntelligenceStudio.Models.RequestModels
+{
+    public class ResumeParserRequest
+    {
+        [Required]
+        public IFormFile File { get; set; }
+    }
+}
---
+++ .Net/AI/AzureDocumentIntelligenceStudio/Services/AzureDocumentIntelligenceService.cs
@@ -0,0 +1,51 @@
+﻿using Azure;
+using Azure.AI.DocumentIntelligence;
+using AzureDocumentIntelligenceStudio.Helpers;
+using AzureDocumentIntelligenceStudio.Models;
+using Microsoft.Extensions.Options;
+
+namespace AzureDocumentIntelligenceStudio.Services
+{
+    public interface IAzureDocumentIntelligenceService
+    {
+        Task<Resume> ResumeExtractionAsync(IFormFile file, string modelId = null, CancellationToken cancellationToken = default);
+    }
+
+    public class AzureDocumentIntelligenceService(DocumentIntelligenceClient _documentIntelligenceClient,
+        IOptions<AzureDocumentIntelligenceConfig> _azureDocumentIntelligenceConfig,
+        IFileHelper _fileHelper,
+        IResumeHelpers _resumeHelper) : IAzureDocumentIntelligenceService
+    {
+        public async Task<Resume> ResumeExtractionAsync(IFormFile file, string? modelId, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(modelId))
+                {
+                    modelId = _azureDocumentIntelligenceConfig.Value.ModelId;
+                }
+
+                if (file == null)
+                    throw new ArgumentNullException("File is empty!!!");
+
+                if (string.IsNullOrWhiteSpace(modelId))
+                    throw new ArgumentNullException("ModelId is not found!!!");
+
+                var stream = await _fileHelper.ToMemoryStreamAsync(file, cancellationToken);
+                var binaryData = await BinaryData.FromStreamAsync(stream, cancellationToken);
+                var operation = await _documentIntelligenceClient.AnalyzeDocumentAsync(WaitUntil.Completed, modelId, binaryData);
+
+                var document = operation.Value.Documents.FirstOrDefault();
+                if (document == null)
+                    throw new Exception("Can not process file");
+
+                var resume = _resumeHelper.ExtractResumeData(document);
+                return resume;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong: {ex.Message}");
+            }
+        }
+    }
+}
